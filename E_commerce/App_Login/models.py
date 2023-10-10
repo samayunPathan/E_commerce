4493cf23ac6bcc689760036ca80101273ff93dc6
samayun <samayunp@gmail.com>
2023-10-10 06:52:47 +0600

    first

---
+++ E_commerce/App_Login/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager,AbstractBaseUser,PermissionsMixin
+from django.utils.translation import ugettext_lazy
+
+# Create your models here.
+class MyUserManager(BaseUserManager):
+    def _create_user(self,email,password,**extra_fields):
+        
+        if not email:
+            raise ValueError("Email not set ! ")
+        email=self.normalize_email(email)
+        user=self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
